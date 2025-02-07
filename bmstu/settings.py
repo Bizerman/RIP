9008dd62fdf4b4eb1842fe663bdb3fbfc5e31b75
Bizarreman <lesha-utenkov@rambler.ru>
2025-02-07 21:56:13 +0300

    add files

--- bmstu/settings.py
+++ bmstu/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
 
 ]
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+            'rest_framework.renderers.JSONRenderer',
+        ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
